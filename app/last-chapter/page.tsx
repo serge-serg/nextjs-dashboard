465f811274fde96a5b2ea3fbc50518f075b069f6
serge-serg <srgg67@gmail.com>
2025-07-11 13:03:17 +0300

    Adding page with last and next chapters

---
+++ app/last-chapter/page.tsx
@@ -0,0 +1,14 @@
+export default function LastChapter() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-center p-6">
+      <div className="text-center">
+        <h1 className="text-4xl font-bold mb-4">          
+          Last Chapter: 2
+          <br />
+          Next Chapter: 3
+        </h1>
+      </div>
+    </main>
+  );
+
+}
