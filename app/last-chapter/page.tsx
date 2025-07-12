e122d469a9e3f1f5fcfe724760269b65a03ae6bd
serge-serg <srgg67@gmail.com>
2025-07-12 10:48:50 +0300

    Started using Tailwind Styled Components

--- app/last-chapter/page.tsx
+++ app/last-chapter/page.tsx
@@ -1,14 +1,34 @@
+"use client";
+import Link from "next/link";
+import tw from "tailwind-styled-components";
 export default function LastChapter() {
+  const ChapterLink = tw(Link)`
+    text-blue-900
+    hover:underline
+  `;
+  const chapters = [
+    { title: "Previous Chapter: 3",
+      href: "optimizing-fonts-images" 
+    },
+    { title: "Last Chapter: 4",
+      href: "#" 
+    },
+  ];
   return (
     <main className="flex min-h-screen flex-col items-center justify-center p-6">
       <div className="text-center">
-        <h1 className="text-4xl font-bold mb-4">          
-          Last Chapter: 2
-          <br />
-          Next Chapter: 3
+        <h1 className="text-4xl font-bold mb-4">
+          {chapters.map((chapter, index) => (
+            <ChapterLink
+              key={index}
+              href={`https://nextjs.org/learn/dashboard-app/${chapter.href}`}
+            >
+              {chapter.title}
+              {index < chapters.length - 1 && " | "}
+            </ChapterLink>
+          ))}
         </h1>
       </div>
     </main>
   );
-
 }
