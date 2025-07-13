c15839e21915ba63f4c3e27a9d521f7e212dcd08
serge-serg <srgg67@gmail.com>
2025-07-13 10:14:42 +0300

    Fixing issue with br abd key

--- app/last-chapter/page.tsx
+++ app/last-chapter/page.tsx
@@ -3,29 +3,36 @@ import Link from "next/link";
 import tw from "tailwind-styled-components";
 export default function LastChapter() {
   const ChapterLink = tw(Link)`
+    inline-block
+    my-8
     text-blue-900
     hover:underline
   `;
+  const [prevChapterTitle, nextChapterTitle] = [
+    "3. Optimizing Fonts and Images",
+    "4. Creating Layouts and Pages",
+  ];
+  const [prevChapterLink, nextChapterLink] = [
+    "optimizing-fonts-images",
+    "creating-layouts-and-pages",
+  ];
   const chapters = [
-    { title: "Previous Chapter: 3",
-      href: "optimizing-fonts-images" 
-    },
-    { title: "Last Chapter: 4",
-      href: "#" 
-    },
+    { title: `Prev Chapter ${prevChapterTitle}`, href: prevChapterLink },
+    { title: `Next Chapter: ${nextChapterTitle}`, href: nextChapterLink },
   ];
   return (
     <main className="flex min-h-screen flex-col items-center justify-center p-6">
       <div className="text-center">
         <h1 className="text-4xl font-bold mb-4">
           {chapters.map((chapter, index) => (
-            <ChapterLink
-              key={index}
-              href={`https://nextjs.org/learn/dashboard-app/${chapter.href}`}
-            >
-              {chapter.title}
-              {index < chapters.length - 1 && " | "}
-            </ChapterLink>
+            <>
+              <ChapterLink
+                key={index}
+                href={`https://nextjs.org/learn/dashboard-app/${chapter.href}`}
+              >{chapter.title}
+              </ChapterLink>
+              {index < chapters.length - 1 && <br key={`br${index}`} />}
+            </>
           ))}
         </h1>
       </div>
