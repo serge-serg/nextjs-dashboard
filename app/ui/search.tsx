58048e3ed5bb9fb29f13765cdacb729e04f11900
serge-serg <srgg67@gmail.com>
2025-07-20 14:18:33 +0300

    Playing with searchParams

--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -1,9 +1,23 @@
-'use client';
-import { MagnifyingGlassIcon } from '@heroicons/react/24/outline';
+"use client";
+import { MagnifyingGlassIcon } from "@heroicons/react/24/outline";
+import { useSearchParams, usePathname, useRouter } from 'next/navigation';
 
 export default function Search({ placeholder }: { placeholder: string }) {
+  const searchParams = useSearchParams();
+  const firstParam = searchParams.toString().split("=")[0]; // actually should be "query"
+  const pathname = usePathname();
+  const { replace } = useRouter();
+  console.log({pathname, firstParam});
+
   function handleSearch(term: string) {
-    console.log(term);
+    const params = new URLSearchParams(searchParams);
+    if (term) {
+      params.set(firstParam, term);
+    } else {
+      params.delete(firstParam);
+    }
+    replace(`${pathname}?${params.toString()}`);
+    console.log({term, params: params.toString()});
   }
 
   return (
@@ -12,11 +26,13 @@ export default function Search({ placeholder }: { placeholder: string }) {
         Search
       </label>
       <input
+        id="search"
         className="peer block w-full rounded-md border border-gray-200 py-[9px] pl-10 text-sm outline-2 placeholder:text-gray-500"
         placeholder={placeholder}
         onChange={(e) => {
           handleSearch(e.target.value);
         }}
+        defaultValue={searchParams.get(firstParam)?.toString()}
       />
       <MagnifyingGlassIcon className="absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
     </div>
